c24987e9c3d43472ac92411fae311cfb80c53ea5
artursmlucena <artur.santana.medeiros.de.lucena@ccc.ufcg.edu.br>
2026-04-26 02:49:55 -0300

    Commit do exercício Titanic.

---
+++ titanic.py
@@ -0,0 +1,39 @@
+import pandas as pd 
+from sklearn.neighbors import KNeighborsClassifier as KNN
+from sklearn.model_selection import cross_val_score
+
+# polishing data
+file = pd.read_csv('titanic.csv', encoding= 'latin1', decimal = '.', sep = ',')
+file_clean = file[["Survived", "Pclass", "Sex", "Age"]]
+file_clean["Age"] = file_clean["Age"].fillna(file_clean["Age"].mean())
+file_clean["Sex"] = file_clean["Sex"].map({"male": 0, "female" : 1})
+
+
+predict = file_clean[["Age", "Pclass", "Sex"]]
+results = file_clean["Survived"]
+
+
+from sklearn.metrics import accuracy_score
+import seaborn as sb
+import matplotlib.pyplot as plt
+scores = []
+k_s = []
+for k in range(1, 6, 1):
+    knn = KNN(n_neighbors = k)
+    score = cross_val_score(knn, predict, results, cv=5)
+    scores.append(score.mean())
+    k_s.append(k)
+
+plt.xlabel("K (vizinhos)")
+plt.ylabel("Accuracy")
+plt.title("KNN - Cross Validation")
+plt.plot(k_s, scores)
+plt.savefig('grafico.png')
+
+
+
+
+
+
+
+
